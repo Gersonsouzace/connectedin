99391164d7d24e823f1aab844505e269c917a61b
Gerson <gersonsouzace@gmail.com>
2019-04-18 14:33:08 -0300

    pullbase'

--- connectedin/urls.py
+++ connectedin/urls.py
@@ -14,21 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path
-from connectedin import views
-
-urlpatterns = [
-    path('', views.index),
-=======
 from django.urls import include, path
 
 
 urlpatterns = [
     path('perfis/', include('perfis.urls')),
->>>>>>> e83b480f9fd311d2a0e6c60992b9fb0e0c0b244c
     path('admin/', admin.site.urls),
-
-
-
 ]
--- perfis/views.py
+++ perfis/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-<<<<<<< HEAD
+
 def index(request):
     return HttpResponse('Bem vindo')
-=======
+
 
 def index(request):
     return render(request, 'index.html')
@@ -12,4 +12,4 @@ def index(request):
 
 def exibir(request):
     return render(request, 'perfil.html')
->>>>>>> e83b480f9fd311d2a0e6c60992b9fb0e0c0b244c
+
