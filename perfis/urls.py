50609969486187a4a076dd40c6d8e4279177455a
Gerson <gersonsouzace@gmail.com>
2019-04-18 22:03:06 -0300

    usando class

--- perfis/urls.py
+++ perfis/urls.py
@@ -19,5 +19,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('perfil/', views.exibir, name="perfil")
+    path('<int:perfil_id>/', views.exibir, name="perfil")
 ]
--- perfis/views.py
+++ perfis/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from connectedin.perfis.models import Perfil
 
-
-def index(request):
-    return HttpResponse('Bem vindo')
+#def index(request):
+    #return HttpResponse('Bem vindo')
 
 
 def index(request):
     return render(request, 'index.html')
 
 
-def exibir(request):
+def exibir(request, perfil_id):
+
+    perfil = Perfil()
+    if perfil_id == '1':
+        perfil = Perfil('Gerson', 'gerson@email.com', '997370753', 'Pathfind')
     return render(request, 'perfil.html')
 
